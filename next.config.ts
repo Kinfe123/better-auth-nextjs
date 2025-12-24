2da19d3282e158f59b0fceaafd4411917156a0b8
Kinfe123 <kinfishtech@gmail.com>
2025-12-25 01:22:15 +0300

    chore: studio prod check

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    outputFileTracingIncludes: {
+      '/api/studio': ['./node_modules/better-auth-studio/dist/public/**/*', './node_modules/better-auth-studio/public/**/*'],
+    },
   /* config options here */
 };
 
